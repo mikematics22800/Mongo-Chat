889a3951133fad925c0fb7f78ff13ff25b64596b
Michael Medina <michaeljmedina22800@gmail.com>
2024-08-25 14:10:32 -0400

    created user and thought models and reaction schema

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/]
+    }, 
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      }
+    ]
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    }
+  }
+);
+
+User.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
